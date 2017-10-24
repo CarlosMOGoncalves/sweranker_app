bea1d1752172a4d7c53be8b89ff360661b78d316
Carlos <carlosmoliveiragoncalves@gmail.com>
2017-10-24 02:25:00 +0100

    Finalised all the degree classes and their structure. Adding Nova Lisboa still in the studies.

--- sweranker/src/main/java/pt/cmg/sweranker/degrees/DegreeAdapter.java
+++ sweranker/src/main/java/pt/cmg/sweranker/degrees/DegreeAdapter.java
@@ -1,16 +1,13 @@
 package pt.cmg.sweranker.degrees;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -55,25 +52,6 @@ public class DegreeAdapter extends RecyclerView.Adapter<DegreeAdapter.DegreeView
         holder._degreeImage.setImageDrawable(_context.getResources().getDrawable(degree.getImageResource(), null));
         holder._degreeImage.setTransitionName("degree_image" + position);
 
-        // TODO: as more degrees start to become available check this!
-        if (position > 2) {
-            holder._degreeImage.setAlpha(0.2f);
-
-            // Ok, here I basically create a TextView to overlay on top of the degrees that are not yet available. Neat.
-            TextView comingSoonText = new TextView(_context);
-            comingSoonText.setText(_context.getString(R.string.coming_soon));
-            comingSoonText.setTextSize(_context.getResources().getDimensionPixelSize(R.dimen.degree_master_comming_soon_text_size));
-            comingSoonText.setRotation(-45f);
-            comingSoonText.setTextColor(ContextCompat.getColor(_context, R.color.materialNegative));
-            comingSoonText.setGravity(Gravity.CENTER);
-
-            ViewGroup.LayoutParams params = holder._imageContainer.getLayoutParams();
-            params.height = FrameLayout.LayoutParams.MATCH_PARENT;
-            params.width = FrameLayout.LayoutParams.MATCH_PARENT;
-
-            holder._imageContainer.addView(comingSoonText, params);
-
-        }
     }
 
 
@@ -100,14 +78,9 @@ public class DegreeAdapter extends RecyclerView.Adapter<DegreeAdapter.DegreeView
             _degreeName = view.findViewById(R.id.degree_name);
             _universityName = view.findViewById(R.id.university_name);
 
-            view.setOnClickListener(v -> {
-                // TODO: as more degrees start to become available check this!
-                if (getAdapterPosition() > 2) {
-                    Toast.makeText(_context, "Coming soon!", Toast.LENGTH_SHORT).show();
-                } else {
-                    _listener.loadDetailedDegreeFragment(v, _degrees.get(getAdapterPosition()));
-                }
-            });
+            view.setOnClickListener(v ->
+                    _listener.loadDetailedDegreeFragment(v, _degrees.get(getAdapterPosition()))
+            );
 
         }
     }
